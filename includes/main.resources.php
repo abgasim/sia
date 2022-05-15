71685a30e33f67a534c9a650adb4dc92ea3edaf2
Abiy Terefe Teshome <abiy.terefe.teshome@ericsson.com>
2022-05-15 14:35:57 +0300

    search by id 25

--- includes/main.resources.php
+++ includes/main.resources.php
@@ -1,15 +1,19 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'].'/includes/main.includes.php';;
+require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/main.includes.php';;
 myheader();
 ?>
-    </div>
-    </div>
-    <div class="middle-section">
-        <div class="container-fluid">
-            <p class="h1">coming Soon</p>
+</div>
+</div>
+<div class="middle-section">
+    <div class="container-fluid">
+        <div class="col-md-12">
+            <div class="col-md-6" style="text-align: justify;height:500px; padding:20px">
+                Coming soon
+            </div>
         </div>
     </div>
-    </body>
+</div>
+</body>
 <?php
 myfooter();
 ?>
--- includes/main.student.portal.php
+++ includes/main.student.portal.php
@@ -1,28 +1,33 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'].'/includes/main.includes.php';;
+require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/main.includes.php';;
 myheader();
 ?>
+
 <body class="login">
     <div class="middle-section">
         <div class="container-fluid">
-            <div class="col-md-4" style="padding: 30px; border:1px solid #c7c7c7; margin:20px;">
-            <form class="form-horizontal" action="./production/index1.php" method="post" enctype="multipart/form-data">
-                <h1>Student Portal</h1>
-                <p class="h6 text-muted">Email</p>
-                <div>
-                    <input type="email" class="form-control" name="employeeID" placeholder="email" required="" />
+            <div class="col-md-12">
+                <div class="col-md-6" style="text-align: justify;height:500px; padding:20px">
+                    <div class="col-md-4" style="padding: 30px; border:1px solid #c7c7c7; margin:20px;">
+                        <form class="form-horizontal" action="./production/index1.php" method="post" enctype="multipart/form-data">
+                            <h1>Student Portal</h1>
+                            <p class="h6 text-muted">Email</p>
+                            <div>
+                                <input type="email" class="form-control" name="employeeID" placeholder="email" required="" />
+                            </div>
+                            <br>
+                            <div>
+                                <input type="password" class="form-control" name="employeePassword" placeholder="Password" required="" />
+                            </div>
+                            <br>
+                            <div>
+                                <button name="submit" type="submit" value="submit" class="btn btn btn-primary">
+                                    Login
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <br>
-                <div>
-                    <input type="password" class="form-control" name="employeePassword" placeholder="Password" required="" />
-                </div>
-                <br>
-                <div>
-                    <button name="submit" type="submit" value="submit" class="btn btn btn-primary">
-                        Login
-                    </button>
-                </div>
-            </form>
             </div>
         </div>
     </div>
