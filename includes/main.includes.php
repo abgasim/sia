ca34c7b9c64734b48668adfda9dbfe28bec943db
Abiy Terefe Teshome <abiy.terefe.teshome@ericsson.com>
2022-05-15 13:52:42 +0300

    search by id 12

--- includes/main.includes.php
+++ includes/main.includes.php
@@ -41,16 +41,19 @@ function myheader()
     <ul>
     <li>
     <a class="h3 links" href = "../includes/main.institutes.gov.php"><i class="fa fa-institution"></i>PTC List</a>
+
+    </li>
+    <li>
     <div class="dropdown">
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      Dropdown button
-    </button>
-    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-      <a class="dropdown-item" href="#">Action</a>
-      <a class="dropdown-item" href="#">Another action</a>
-      <a class="dropdown-item" href="#">Something else here</a>
-    </div>
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown button
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
   </div>
+</div>
     </li>
     <li><a class="h3 links" href = "../includes/main.aboutus.php">About Us</a></li>
 
