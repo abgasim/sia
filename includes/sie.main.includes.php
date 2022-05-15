5be3c708d7cbcbe6d1fb3be1509227c27979197f
Abiy Terefe Teshome <abiy.terefe.teshome@ericsson.com>
2022-05-15 06:52:42 +0300

    fix css folder

--- includes/sie.main.includes.php
+++ includes/sie.main.includes.php
@@ -27,7 +27,7 @@ function sie_header()
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../sie_css/sie.css">
+    <link rel="stylesheet" href="../css/sie.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5528376568579945"
      crossorigin="anonymous"></script>
