8a8f12b53486cb427082b62e811136bb66fdc86c
Abiy Terefe Teshome <abiy.terefe.teshome@ericsson.com>
2022-05-15 13:57:17 +0300

    search by id 14

--- includes/main.includes.php
+++ includes/main.includes.php
@@ -44,12 +44,8 @@ function myheader()
 
     </li>
     <li>
-    <div class="container">
-  <h2>Dropdowns</h2>
-  <p>The .dropdown class is used to indicate a dropdown menu.</p>
-  <p>Use the .dropdown-menu class to actually build the dropdown menu.</p>
-  <p>To open the dropdown menu, use a button or a link with a class of .dropdown-toggle and data-toggle="dropdown".</p>                                          
-  <div class="dropdown">
+  
+    <div class="dropdown">
     <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
     <span class="caret"></span></button>
     <ul class="dropdown-menu">
@@ -57,7 +53,6 @@ function myheader()
       <li><a href="#">CSS</a></li>
       <li><a href="#">JavaScript</a></li>
     </ul>
-  </div>
 </div>
     </li>
     <li><a class="h3 links" href = "../includes/main.aboutus.php">About Us</a></li>
