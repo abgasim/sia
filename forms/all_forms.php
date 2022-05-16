bb783396d591bb3f5cc2550411f6d558f343fc8d
Abiy Terefe Teshome <abiy.terefe.teshome@ericsson.com>
2022-05-16 16:01:55 +0300

    form fix css

--- forms/all_forms.php
+++ forms/all_forms.php
@@ -3,14 +3,13 @@ require_once '../includes/main.connect.php';
 require_once '../includes/main.includes.php';
 myheader();
 ?>
-
 <body>
     <div class="middle-section">
         <div class="container-fluid">
             <div class="col-md-12">
-                <a href="new_ptc_form.php">New ptc form</a>
-                <a href="new_occupation_form.php">New occupation form</a>
-                <a href="ptc_occupation_form.php">ptc occupation form</a>
+                <a href="new_ptc_form.php"><div class="col-med-4">New ptc form</div></a>
+                <a href="new_occupation_form.php"><div class="col-md-4">New occupation form</div></a>
+                <a href="ptc_occupation_form.php"><div class="col-md-4">ptc occupation form</div></a>
             </div>
         </div>
     </div>
