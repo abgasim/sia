d41a8ecf40a8d9e04926c0901c78ced4ba724d71
Abiy Terefe Teshome <abiy.terefe.teshome@ericsson.com>
2022-05-16 16:04:49 +0300

    form fix css 1

--- forms/all_forms.php
+++ forms/all_forms.php
@@ -7,9 +7,9 @@ myheader();
     <div class="middle-section">
         <div class="container-fluid">
             <div class="col-md-12">
-                <a href="new_ptc_form.php"><div class="col-med-4">New ptc form</div></a>
-                <a href="new_occupation_form.php"><div class="col-md-4">New occupation form</div></a>
-                <a href="ptc_occupation_form.php"><div class="col-md-4">ptc occupation form</div></a>
+                <a href="new_ptc_form.php"><div class="col-md-4 panel panel-default">New ptc form</div></a>
+                <a href="new_occupation_form.php"><div class="col-md-4 panel panel-default">New occupation form</div></a>
+                <a href="ptc_occupation_form.php"><div class="col-md-4 panel panel-default">ptc occupation form</div></a>
             </div>
         </div>
     </div>
