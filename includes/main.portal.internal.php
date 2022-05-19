580c4f96d05ab600df52934b4a9ca320f984f907
Abiy Terefe Teshome <abiy.terefe.teshome@ericsson.com>
2022-05-19 14:36:48 +0300

    portal 4

--- includes/main.portal.internal.php
+++ includes/main.portal.internal.php
@@ -10,18 +10,27 @@ myheader();
                 <div class="col-md-6" style="text-align: justify;height:500px; padding:20px">
                     <form class="form-horizontal" action="#" method="post" enctype="multipart/form-data">
                         <h1>Internal Portal</h1>
-                        <div>
-                            <input type="email" class="form-control" name="userID" placeholder="email" required="" />
+                        <div class="form-group row">
+                            <label for="text" class="col-4 col-form-label">Email</label>
+                            <div class="col-8">
+                                <input type="email" class="form-control" name="userID" placeholder="" required="" />
+                            </div>
                         </div>
                         <br>
-                        <div>
-                            <input type="password" class="form-control" name="userPassword" placeholder="Password" required="" />
+                        <div class="form-group row">
+                            <label for="text" class="col-4 col-form-label">Password</label>
+                            <div class="col-8">
+
+                                <input type="password" class="form-control" name="userPassword" placeholder="" required="" />
+                            </div>
                         </div>
                         <br>
-                        <div>
-                            <button name="submit" type="submit" value="submit" class="btn btn btn-primary">
-                                Login
-                            </button>
+                        <div class="form-group row">
+                            <div class="offset-4 col-8">
+                                <button name="submit" type="submit" value="submit" class="btn btn btn-primary">
+                                    Login
+                                </button>
+                            </div>
                         </div>
                     </form>
                 </div>
--- includes/main.portal.student.php
+++ includes/main.portal.student.php
@@ -11,34 +11,43 @@ myheader();
                     <form class="form-horizontal" action="#" method="post" enctype="multipart/form-data">
                         <h1>Student Portal</h1>
                         <div class="form-group row">
-                            <select id="student-portal-select" name="select" class="custom-select form-control">
-                               <option value="000">Select Your Polytechnic College</option>
-                                <?
-                                global $lang;
-                                $sql = "SELECT * FROM ptc_name ORDER BY name";
-                                $data = $connect->query($sql);
-                                $total = $data->rowCount();
-                                foreach ($data as $row) {
-                                    echo '<option value="' . $row["logo"] . '">' . $_TXT[$row['name']] . '&nbsp;[' . $row["logo"] . ']</option>';
-                                }
-                                ?>
-                            </select>
+                            <label for="select" class="col-4 col-form-label">Select</label>
+                            <div class="col-8">
+                                <select id="student-portal-select" name="select" class="custom-select form-control">
+                                    <option value="000">Select Your Polytechnic College</option>
+                                    <?
+                                    global $lang;
+                                    $sql = "SELECT * FROM ptc_name ORDER BY name";
+                                    $data = $connect->query($sql);
+                                    $total = $data->rowCount();
+                                    foreach ($data as $row) {
+                                        echo '<option value="' . $row["logo"] . '">' . $_TXT[$row['name']] . '&nbsp;[' . $row["logo"] . ']</option>';
+                                    }
+                                    ?>
+                                </select>
+                            </div>
                         </div>
                         <div class="form-group row">
-
-                            <input type="email" class="form-control" name="userID" placeholder="email" required="" />
+                            <label for="text" class="col-4 col-form-label">Email</label>
+                            <div class="col-8">
+                                <input type="email" class="form-control" name="userID" placeholder="" required="" />
+                            </div>
                         </div>
                         <br>
                         <div class="form-group row">
+                            <label for="text" class="col-4 col-form-label">Password</label>
+                            <div class="col-8">
 
-                            <input type="password" class="form-control" name="userPassword" placeholder="Password" required="" />
+                                <input type="password" class="form-control" name="userPassword" placeholder="" required="" />
+                            </div>
                         </div>
                         <br>
                         <div class="form-group row">
-
-                            <button name="submit" type="submit" value="submit" class="btn btn btn-primary">
-                                Login
-                            </button>
+                            <div class="offset-4 col-8">
+                                <button name="submit" type="submit" value="submit" class="btn btn btn-primary">
+                                    Login
+                                </button>
+                            </div>
                         </div>
                     </form>
                 </div>
